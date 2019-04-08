6535d92bb80eba06a48154e77d06b8b524d8c431
xianchen <super_cx@163.com>
2019-04-08 16:23:24 +0800

    cs

--- src/main/java/com/company/project/controller/BeautifulGirlContrller.java
+++ src/main/java/com/company/project/controller/BeautifulGirlContrller.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping
 public class BeautifulGirlContrller {
 
+	//csdcsdac
     @Value("${cubSize}")
     private String cubSize;
 
